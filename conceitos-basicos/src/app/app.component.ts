3c70af9988a87652398301efe09f38623608146a
henriquejunqueira <henriquejunqueira15@gmail.com>
2025-12-24 09:32:49 -0300

    Criando uma lista de compras simples

--- conceitos-basicos/src/app/app.component.ts
+++ conceitos-basicos/src/app/app.component.ts
@@ -3,11 +3,13 @@ import { Component } from '@angular/core';
 // import { MinhapaginaComponent } from './minhapagina/minhapagina.component';
 // import { HelloWorldComponent } from './helloworld/helloworld.component';
 import { CalculadoraComponent } from './calculadora/calculadora.component';
+import { ListaComprasComponent } from './lista-compras/lista-compras.component';
 
 @Component({
   selector: 'app-root',
   // imports: [HelloWorldComponent, MinhapaginaComponent],
-  imports: [CalculadoraComponent],
+  // imports: [CalculadoraComponent],
+  imports: [ListaComprasComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
