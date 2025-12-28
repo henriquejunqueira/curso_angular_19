3ce181ac261731b6efb879a1c348fabe84acef6e
henriquejunqueira <henriquejunqueira15@gmail.com>
2025-12-28 08:39:20 -0300

    Router link e material icon

--- crud-angular-material/src/app/app.routes.ts
+++ crud-angular-material/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { ConsultaComponent } from './consulta/consulta.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // Define as rotas dos componentes
+  {
+    path: 'cadastro',
+    component: CadastroComponent,
+  },
+  {
+    path: 'consulta',
+    component: ConsultaComponent,
+  },
+];
