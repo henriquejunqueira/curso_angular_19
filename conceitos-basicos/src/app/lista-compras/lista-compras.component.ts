95a5fbc60920061b1fd64b7fceb44d397ba69b02
henriquejunqueira <henriquejunqueira15@gmail.com>
2025-12-27 22:48:29 -0300

    Logica de riscar itens e classe css dinamica no componente

--- conceitos-basicos/src/app/lista-compras/lista-compras.component.ts
+++ conceitos-basicos/src/app/lista-compras/lista-compras.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ItemLista } from './itemlista';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-lista-compras',
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './lista-compras.component.html',
   styleUrl: './lista-compras.component.scss',
 })
@@ -23,5 +24,13 @@ export class ListaComprasComponent {
     this.item = '';
 
     console.table(this.lista);
+  }
+
+  riscarItem(itemLista: ItemLista) {
+    itemLista.comprado = !itemLista.comprado;
+  }
+
+  limparLista() {
+    this.lista = [];
   }
 }
